ddc21168a7d35aa8e8df40f70a106b86aa281527
Garfild13-06 <Garfild13-06@yandex.com>
2023-08-23 12:48:13 +0300

    23.08.23 12:48

--- intro/views.py
+++ intro/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
 
+
 # Create your views here.
 # MVC - Model-View-Controller
 # MTV - Model-Template-View
@@ -11,8 +12,10 @@ def hello_view(request):
     # ip=request.META.get('HTTP_REFERER')
     return HttpResponse(f"hello, world")
 
+
 def time_view(request):
     return HttpResponse(datetime.datetime.now().isoformat())
 
+
 def index_view(request):
-    return HttpResponse("<br>".join([reverse("h2"),reverse("t"), reverse("i")]))
+    return HttpResponse("<br>".join([reverse("h2"), reverse("t"), reverse("i")]))
